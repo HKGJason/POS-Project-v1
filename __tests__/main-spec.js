d07d6837f90b8cf714f78870ef931fc1095da2ea
J <jason19951020@gmail.com>
2018-11-21 22:17:37 +0800

    code for getpromote

--- __tests__/main-spec.js
+++ __tests__/main-spec.js
@@ -2,7 +2,7 @@ const printReceipt = require('../main');
 
 it ('To test output recipe', () => {
 	var expected = "***<store earning no money>Receipt ***\n";
-	var item = ['ITEM000001','ITEM000001'];
+	var item = ['ITEM000001','ITEM000002'];
 	var actual = printReceipt.printReceipt(item);
 	expect(actual).toBe(expected);
 });
--- main.js
+++ main.js
@@ -3,16 +3,31 @@ function printReceipt(boughtItem){
 	var allitem = loadAllItems();
 	var allPromote = loadPromotions();
 	var eachItem = getBroughtItem(allitem, boughtItem);
+	eachItem = getPromotions(eachItem,allPromote)
 	return eachItem;
 	//return recipeStr;
+}
+function getPromotions(item,promote){
+	promote.forEach(p=>{
+		if(p.type === 'BUY_TWO_GET_ONE_FREE')
+		{
+			item.filter(i=>p.barcodes.includes(i.barcode)).filter(i=>i.quantity>=3).map(j=>j.save = j.price)
+		};
+	});
+	return item;
 }
 function getBroughtItem(allitem, boughtItem){
 	let eachItem = allitem.filter(a=>boughtItem.includes(a.barcode));
 	eachItem.map(a=>a.quantity = 0);
 	for (let index = 0; index<boughtItem.length;index++){
 		for (let jndex = 0; jndex<eachItem.length;jndex++){
-			if (boughtItem[index]===eachItem[jndex].barcode)
-				eachItem[jndex].quantity=eachItem[jndex].quantity+1;
+			if (boughtItem[index].includes(eachItem[jndex].barcode)){
+				if ((boughtItem[index].length)>10){
+					eachItem[jndex].quantity=eachItem[jndex].quantity+2;
+				}else{
+					eachItem[jndex].quantity=eachItem[jndex].quantity+1;
+				}
+			}
 		}
 	}
 	return eachItem;
@@ -62,7 +77,7 @@ function loadAllItems() {
 function loadPromotions() {
   return [
     {
-      type: 'BUY_TWO_GET_ONE_FREE',
+	  type: 'BUY_TWO_GET_ONE_FREE',
       barcodes: [
         'ITEM000000',
         'ITEM000001',
